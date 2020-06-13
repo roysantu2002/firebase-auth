7c5a0058d97ec28acca102f6ece08ab92aa1de93
Santu Roy <roysantu2002@yahoo.com>
2020-06-13 07:28:26 +0530

    feat: added toolbar, logo using material ui and theme

--- src/components/UI/AppTheme.js
+++ src/components/UI/AppTheme.js
@@ -1,18 +1,27 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
-const lightBlue = "#7395AE"
-const darkGery = "#5D5C61"
+const lightBlue = "#7395AE";
+const darkGery = "#5D5C61";
 export default createMuiTheme({
-    palette: {
-        common :{
-            blue: `${lightBlue}`,
-            grey : `${darkGery}`
-        },
-        primary :{
-            main : `${lightBlue}`
-        },
-        secondary: {
-            main : `${darkGery}`
-        }
-    }
+  palette: {
+    common: {
+      blue: `${lightBlue}`,
+      grey: `${darkGery}`,
+    },
+    primary: {
+      main: `${lightBlue}`,
+    },
+    secondary: {
+      main: `${darkGery}`,
+    },
+  },
+  typography: {
+    h3: {
+      fontFamily: "Roboto",
+      fontWeight: 300,
+      fontSize: "6rem",
+      lineHeight: 1.167,
+      letterSpacing: "-0.01562em",
+    },
+  },
 });
--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -2,26 +2,58 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import logo from "../../assets/SF.png"
 
 function ElevationScroll(props) {
-    const { children } = props;
+  const { children } = props;
 
-    const trigger = useScrollTrigger({
-      disableHysteresis: true,
-      threshold: 0,
-    });
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+const useStyles = makeStyles((theme) => ({
+  toobarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: "3em"
+  },
+  logo:{
+    
+    height:"4em",
+    padding: "0.5em"
   
-    return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
-    });
   }
+}));
+
+const imgStyle = makeStyles({
+  img: {
+   
+    height: 50,
+    width: 50
+   
+  },
+});
 
 export default function Header(props) {
+  const classes = useStyles();
+  const imgClass = imgStyle();
   return (
-    <ElevationScroll>
-      <AppBar position='fixed'>
-        <Toolbar>wwww.localsbot.com</Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar position='fixed' color='primary'>
+          <Toolbar disableGutters="false">
+           <img src={logo} alt='logo' className={classes.logo}></img>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toobarMargin} />
+    </React.Fragment>
   );
 }
