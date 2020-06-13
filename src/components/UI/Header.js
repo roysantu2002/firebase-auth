0d61fdc405d8af316a24e4ba44ea39d16ae125dc
Santu Roy <roysantu2002@yahoo.com>
2020-06-13 08:30:35 +0530

    feat: added toolbar's tabs and tab

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,11 +1,23 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import PhoneIcon from "@material-ui/icons/Phone";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import PersonPinIcon from "@material-ui/icons/PersonPin";
+import HelpIcon from "@material-ui/icons/Help";
+import ShoppingBasket from "@material-ui/icons/ShoppingBasket";
+import ThumbDown from "@material-ui/icons/ThumbDown";
+import ThumbUp from "@material-ui/icons/ThumbUp";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Box from "@material-ui/core/Box";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import logo from "../../assets/SF.png"
+import logo from "../../assets/SF.png";
 
+//Header elevator
 function ElevationScroll(props) {
   const { children } = props;
 
@@ -17,39 +29,135 @@ function ElevationScroll(props) {
   return React.cloneElement(children, {
     elevation: trigger ? 4 : 0,
   });
+} //end of header elevator
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role='tabpanel'
+      hidden={value !== index}
+      id={`scrollable-force-tabpanel-${index}`}
+      aria-labelledby={`scrollable-force-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
 }
 
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-force-tab-${index}`,
+    "aria-controls": `scrollable-force-tabpanel-${index}`,
+  };
+}
+//tabs functoions
+//Styles
 const useStyles = makeStyles((theme) => ({
   toobarMargin: {
     ...theme.mixins.toolbar,
-    marginBottom: "3em"
+    marginBottom: "3em",
   },
-  logo:{
-    
-    height:"4em",
-    padding: "0.5em"
-  
-  }
+  logo: {
+    height: "4em",
+    padding: "0.5em",
+  },
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    fontFamily: "",
+    textTransform: "none",
+    fontWeight: 700,
+    fontSize: "1rem",
+    minWidth: 10,
+    marginLeft: "25",
+  },
 }));
 
-const imgStyle = makeStyles({
-  img: {
-   
-    height: 50,
-    width: 50
-   
-  },
-});
+// const imgStyle = makeStyles({
+//   img: {
+//     height: 50,
+//     width: 50,
+//   },
+// });
 
 export default function Header(props) {
   const classes = useStyles();
-  const imgClass = imgStyle();
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar position='fixed' color='primary'>
-          <Toolbar disableGutters="false">
-           <img src={logo} alt='logo' className={classes.logo}></img>
+          <Toolbar disableGutters={false}>
+            <img src={logo} alt='logo' className={classes.logo}></img>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              variant='scrollable'
+              scrollButtons='on'
+              indicatorColor='primary'
+              textColor='primary'
+              aria-label='scrollable force tabs example'
+            >
+              <Tab
+                
+                label='Item One'
+                icon={<PhoneIcon />}
+                {...a11yProps(0)}
+              />
+              <Tab
+              
+                label='Item Two'
+                icon={<FavoriteIcon />}
+                {...a11yProps(1)}
+              />
+              <Tab
+              
+                label='Item Three'
+                icon={<PersonPinIcon />}
+                {...a11yProps(2)}
+              />
+              <Tab
+              
+                label='Item Four'
+                icon={<HelpIcon />}
+                {...a11yProps(3)}
+              />
+              <Tab
+              
+                label='Item Five'
+                icon={<ShoppingBasket />}
+                {...a11yProps(4)}
+              />
+              <Tab
+               
+                label='Item Six'
+                icon={<ThumbDown />}
+                {...a11yProps(5)}
+              />
+              <Tab
+                
+                label='Item Seven'
+                icon={<ThumbUp />}
+                {...a11yProps(6)}
+              />
+            </Tabs>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
