d3f2ab342c509c962b385e2e25dd7d7a2aa73666
Santu Roy <roysantu2002@yahoo.com>
2020-06-13 20:25:06 +0530

    feat: adding menu and menu items and submenu with switch statement

--- src/components/App.js
+++ src/components/App.js
@@ -12,12 +12,12 @@ function App() {
         <Header/>
         <Switch>
           <Route exact path="/"  component={() => <div>Home</div>}></Route>
-          <Route exact path="/Coding"  component={() => <div>Coding</div>}></Route>
-          <Route exact path="/Writing"  component={() => <div>Writing</div>}></Route>
+          <Route exact path="/Offered"  component={() => <div>Offered</div>}></Route>
+          {/* <Route exact path="/Writing"  component={() => <div>Writing</div>}></Route>
           <Route exact path="/Filmmaking"  component={() => <div>Filmmaking</div>}></Route>
           <Route exact path="/Robotics"  component={() => <div>Robotics</div>}></Route>
           <Route exact path="/AppMaking"  component={() => <div>AppMaking</div>}></Route>
-          <Route exact path="/Baking"  component={() => <div>Baking</div>}></Route>
+          <Route exact path="/Baking"  component={() => <div>Baking</div>}></Route> */}
           <Route exact path="/About"  component={() => <div>About</div>}></Route>
           <Route exact path="/Contact"  component={() => <div>Contact</div>}></Route>
         </Switch>
--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from "react"
-import PropTypes from "prop-types"
-import AppBar from "@material-ui/core/AppBar"
-import Toolbar from "@material-ui/core/Toolbar"
-import Tabs from "@material-ui/core/Tabs"
-import Tab from "@material-ui/core/Tab"
-import PhoneIcon from "@material-ui/icons/Phone"
-import FavoriteIcon from "@material-ui/icons/Favorite"
-import PersonPinIcon from "@material-ui/icons/PersonPin"
-import HelpIcon from "@material-ui/icons/Help"
-import ShoppingBasket from "@material-ui/icons/ShoppingBasket"
-import ThumbDown from "@material-ui/icons/ThumbDown"
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import PhoneIcon from "@material-ui/icons/Phone";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import PersonPinIcon from "@material-ui/icons/PersonPin";
+import HelpIcon from "@material-ui/icons/Help";
+import ShoppingBasket from "@material-ui/icons/ShoppingBasket";
+import ThumbDown from "@material-ui/icons/ThumbDown";
 import ThumbUp from "@material-ui/icons/ThumbUp";
-import useScrollTrigger from "@material-ui/core/useScrollTrigger"
-import Box from "@material-ui/core/Box"
-import { makeStyles } from "@material-ui/core/styles"
-import Typography from "@material-ui/core/Typography"
-import logo from "../../assets/SF.png"
-import { Link } from "react-router-dom"
-import Button from "@material-ui/core/Button"
-import Menu from "@material-ui/core/Menu"
-import MenuItem from "@material-ui/core/MenuItem"
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import logo from "../../assets/SF.png";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 //Header elevator
 function ElevationScroll(props) {
@@ -78,11 +78,11 @@ const useStyles = makeStyles((theme) => ({
     height: "4em",
     padding: "0.5em",
   },
-  logoContainer:{
+  logoContainer: {
     padding: 0,
     "&:hover": {
-      backgroundColor: "transparent"
-    }
+      backgroundColor: "transparent",
+    },
   },
   tabContainer: {
     marginLeft: "auto",
@@ -90,7 +90,19 @@ const useStyles = makeStyles((theme) => ({
   tab: {
     ...theme.typography.tab,
     minWidth: 10,
-    marginLeft: "10",
+    marginLeft: "100",
+  },
+  menu: {
+    backgroundColor: theme.palette.common.grey,
+    color: theme.palette.common.blue,
+    borderRadius: "0px",
+  },
+  menuItem: {
+    ...theme.typography.tab,
+    opacity: 0.7,
+    "&:hover": {
+      opacity: 1,
+    },
   },
 }));
 
@@ -102,10 +114,11 @@ const useStyles = makeStyles((theme) => ({
 // });
 
 export default function Header(props) {
-  const classes = useStyles()
-  const [value, setValue] = useState(0)
-  const [anchorEl, setAnchorEl] = useState(null)
-  const [open, setOpen] = useState(false)
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -113,21 +126,72 @@ export default function Header(props) {
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
-    setOpen(true)
+    setOpen(true);
   };
 
   const handleClose = () => {
-    setAnchorEl(null)
-    setOpen(false)
+    setAnchorEl(null);
+    setOpen(false);
   };
 
+  const manuOptions = [
+    { name: "Offered", Link: "/Offered" },
+    { name: "Writing", Link: "Writing" },
+    { name: "Robotics", Link: "Robotics" },
+  ];
+
+  const handleMenuItemClick = (event, i) => {
+    setAnchorEl(null);
+    setOpen(false);
+    setSelectedIndex(i);
+  };
   useEffect(() => {
-    if (window.location.pathname === "/" && value !== 0) {
-      setValue(0);
-    } else if (window.location.pathname === "/Coding" && value !== 1) {
-      setValue(1);
-    } else if (window.location.pathname === "/Writing" && value !== 2) {
-      setValue(2);
+    // if (window.location.pathname === "/" && value !== 0) {
+    //   setValue(0);
+    // } else if (window.location.pathname === "/Coding" && value !== 1) {
+    //   setValue(1);
+    // } else if (window.location.pathname === "/Writing" && value !== 2) {
+    //   setValue(2);
+    // }
+
+    switch (window.location.pathname) {
+      case "/":
+        if (value !== 0) {
+          setValue(0);
+        }
+        break;
+      case "/Offered":
+        if (value !== 1) {
+          setValue(1);
+         setSelectedIndex(0)
+        }
+        break;
+        case "/Writing":
+          if (value !== 1) {
+            setValue(1);
+           setSelectedIndex(1)
+          }
+          break;
+          case "/Robotics":
+            if (value !== 1) {
+              setValue(1);
+             setSelectedIndex(2)
+            }
+            break;
+      case "/About":
+        if (value !== 2) {
+          setValue(2);
+         // setSelectedIndex(1)
+        }
+        break;
+      case "/Contact":
+        if (value !== 3) {
+          setValue(3);
+         // setSelectedIndex(2)
+        }
+        break;
+      default:
+        break;
     }
   }, [value]);
 
@@ -136,7 +200,13 @@ export default function Header(props) {
       <ElevationScroll>
         <AppBar position='fixed' color='secondary'>
           <Toolbar disableGutters={false}>
-            <Button disableRipple className={classes.logoContainer} component={Link} to='/' onClick={() => setValue(0)}>
+            <Button
+              disableRipple
+              className={classes.logoContainer}
+              component={Link}
+              to='/'
+              onClick={() => setValue(0)}
+            >
               <img src={logo} alt='logo' className={classes.logo}></img>
             </Button>
 
@@ -158,33 +228,33 @@ export default function Header(props) {
                 {...a11yProps(0)}
               />
               <Tab
-              aria-owns={anchorEl ? "We offer" : undefined}
-              aria-haspopup={anchorEl ? "true" : undefined}
+                aria-owns={anchorEl ? "We offer" : undefined}
+                aria-haspopup={anchorEl ? "true" : undefined}
                 label='Offered'
-                to='/Coding'
+                to='/Offered'
                 component={Link}
-                onClick={(event) => handleClick}
+                onMouseOver={(event) => handleClick(event)}
                 icon={<FavoriteIcon />}
                 {...a11yProps(1)}
               />
               <Tab
-                label='Item Three'
-                to='/Writing'
+                label='About'
+                to='/About'
                 component={Link}
                 icon={<PersonPinIcon />}
                 {...a11yProps(2)}
               />
               <Tab
-                label='Item Four'
-                to='/Filmmaking'
+                label='Contact'
+                to='/Contact'
                 component={Link}
                 icon={<HelpIcon />}
                 {...a11yProps(3)}
               />
-              <Tab
+              {/* <Tab
                 label='Item Five'
-                to='/Robotics'
                 component={Link}
+                to='/Robotics'
                 icon={<ShoppingBasket />}
                 {...a11yProps(4)}
               />
@@ -201,9 +271,44 @@ export default function Header(props) {
                 component={Link}
                 icon={<ThumbUp />}
                 {...a11yProps(6)}
-              />
+              /> */}
             </Tabs>
           </Toolbar>
+          <Menu
+            id='simple-menu'
+            anchorEl={anchorEl}
+            keepMounted
+            open={open}
+            onClose={handleClose}
+            classes={{ paper: classes.menu }}
+            MenuListProps={{ onMouseLeave: handleClose }}
+            elevation={0}
+          >
+            {manuOptions.map((option, i) => (
+              <MenuItem
+                key={option}
+                component={Link}
+                to={option.Link}
+                classes={{ root: classes.menuItem }}
+                onClick={(event) => {
+                  handleMenuItemClick(event, i);
+                  setValue(1);
+                  handleClose();
+                }}
+                selected={i === selectedIndex && value === 1}
+              >
+                {option.name}
+              </MenuItem>
+            ))}
+            {/* <MenuItem onClick={() => {handleClose(); setValue(1)}}  component={Link}
+                to='/Offered' classes={{root: classes.menuItem}}>Offered</MenuItem>
+            <MenuItem onClick={() => {handleClose(); setValue(1)}}  component={Link}
+                to='/Robotics' classes={{root: classes.menuItem}}>Robotics</MenuItem>
+            <MenuItem onClick={() => {handleClose(); setValue(1)}}  component={Link}
+                to='/AppMaking' classes={{root: classes.menuItem}}>AppMaking</MenuItem>
+            <MenuItem onClick={() => {handleClose(); setValue(1)}}  component={Link}
+                to='/Baking' classes={{root: classes.menuItem}}>Baking</MenuItem> */}
+          </Menu>
         </AppBar>
       </ElevationScroll>
       <div className={classes.toobarMargin} />
