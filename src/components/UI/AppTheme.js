cf18fad1a2b398e7e81acaa3835948d8a3419606
Santu Roy <roysantu2002@yahoo.com>
2020-06-27 06:18:05 +0530

    Feat: modified theme file

--- src/components/UI/AppTheme.js
+++ src/components/UI/AppTheme.js
@@ -1,33 +1,59 @@
 import { createMuiTheme } from "@material-ui/core/styles";
+import { green, grey, red } from "@material-ui/core/colors";
 
-const red = "#ff3422"
+const darkRed = "#ff3422"
 const gold = "#835A00"
-const lightBlue = "#051e3e";
+const lightBlue = "#051e3e"
 const darkBlue = "#628078"
-
 const appleGrey = "#999999"
+const darkBlack = "#000000"
 
 export default createMuiTheme({
   palette: {
     common: {
-      darkRed: `${red}`,
+      darkRed: `${darkRed}`,
       darkGold: `${gold}`,
       darkBlue: `${darkBlue}`,
-      appleGrey: `${appleGrey}`
+      appleGrey: `${appleGrey}`,
+      darkBlack : `${darkBlack}`
     },
     primary: {
-      main: `${red}`,
+      main: `${darkRed}`,
+      light: "#ff6f4e",
+      dark: "#c30000"
     },
     secondary: {
       main: `${gold}`,
+      light: "#b78735",
+      dark: "#533100",
+    },
+    warning:{
+      main:"#ffc071",
+      dark: "#c99043",
+    },
+    error:{
+      xLight: red[50],
+      main: red[500],
+      dark: red[700]
+    },
+    success: {
+      xLight: green[50],
+      main: green[500],
+      dark: green[700],
     },
     darkBlue:{
       main: `${darkBlue}`
     },
-    appleGrey: `${appleGrey}`
+    appleGrey: `${appleGrey}`,
   },
   typography: {
 
+    fontFamily: "'Muli', 'sans-serif'",
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 700,
+    fontFamilySecondary: "'Philosopher', 'sans-serif'",
     genericFont:{
       fontFamily: 'Muli',
       color: "white",
@@ -40,7 +66,6 @@ export default createMuiTheme({
       fontStyle: "italic",
       color: "white",
       fontSize: ".8rem",
-      // minwidth: 25,
       marginLeft: "5px"
     },
 
